99da660ce3ef38302d10272088c7ce9b0ef5a77c
NazarStf <hworker98@gmail.com>
2023-07-28 16:57:34 +0300

    Initial Commit

---
+++ Present and Dismiss Transition/Present_and_Dismiss_TransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Present_and_Dismiss_TransitionApp.swift
+//  Present and Dismiss Transition
+//
+//  Created by NazarStf on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Present_and_Dismiss_TransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
